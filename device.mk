7ccb2b8aec7785f73aacb1ee35e7a87cbe0890a6
ShazuxD <ShazuxD7@gmail.com>
2023-04-19 10:39:20 +0530

    RMX1801: use prebuilt modules & drop ueventd.rc
    Signed-off-by: ShazuxD <ShazuxD7@gmail.com>

--- device.mk
+++ device.mk
@@ -1,14 +1,5 @@
 LOCAL_PATH := device/realme/RMX1801
 
-# Build from source
-TARGET_RECOVERY_DEVICE_MODULES += \
-    libion \
-    libxml2
-
-TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libion.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libxml2.so
-
 # Crypto
 PRODUCT_PACKAGES += \
     qcom_decrypt \
