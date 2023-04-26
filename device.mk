d5263ef29c34c7012fe2c9202acf18f637c99cb0
ShazuxD <shazuxd7@gmail.com>
2023-04-26 13:53:05 +0530

    RMX1801: copy out files

--- device.mk
+++ device.mk
@@ -7,3 +7,10 @@ PRODUCT_SOONG_NAMESPACES += \
 # Keystore
 PRODUCT_PACKAGES += \
     android.system.keystore2
+
+PRODUCT_COPY_FILES += \
+   $(call find-copy-subdir-files,*,$(LOCAL_PATH)/recovery/root/,out/target/product/realme/RMX1801/recovery/root)
+
+PRODUCT_COPY_FILES += \
+	$(OUT_DIR)/target/product/RMX1801/system/bin/android.hardware.gatekeeper@1.0-service:$(LOCAL_PATH)/recovery/root/system/bin/android.hardware.gatekeeper@1.0-service \
+        $(OUT_DIR)/target/product/RMX1801/system/bin/android.hardware.gatekeeper@1.0-service:$(LOCAL_PATH)/recovery/root/system/bin/android.hardware.keymaster@3.0-service-qti
