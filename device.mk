585b2d7b13f67bdbd6c0ab95a2cebf666df18c94
ShazuxD <shazuxd7@gmail.com>
2023-04-26 12:16:40 +0530

    Revert "RMX1801: let qcom common decryption tree handle decryption"
    This reverts commit bef3652898f46c8412f4469338a0340feecd2d24.

--- device.mk
+++ device.mk
@@ -1,10 +1,5 @@
 LOCAL_PATH := device/realme/RMX1801
 
-# Crypto
-PRODUCT_PACKAGES += \
-    qcom_decrypt \
-    qcom_decrypt_fbe
-
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
