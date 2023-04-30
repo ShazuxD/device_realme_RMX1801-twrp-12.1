ab5cd323f4f8172e6dcaee4e3f3fdd42c012730c
ShazuxD <shazuxd7@gmail.com>
2023-04-30 17:25:09 +0530

    RMX1801: add missing blob for oppo crypto eng & drop km 3

--- device.mk
+++ device.mk
@@ -3,10 +3,3 @@ LOCAL_PATH := device/realme/RMX1801
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
-
-PRODUCT_COPY_FILES += \
-   $(call find-copy-subdir-files,*,$(LOCAL_PATH)/recovery/root/,out/target/product/realme/RMX1801/recovery/root)
-
-PRODUCT_COPY_FILES += \
-	$(OUT_DIR)/target/product/RMX1801/system/bin/android.hardware.gatekeeper@1.0-service:$(LOCAL_PATH)/recovery/root/system/bin/android.hardware.gatekeeper@1.0-service \
-        $(OUT_DIR)/target/product/RMX1801/system/bin/android.hardware.gatekeeper@1.0-service:$(LOCAL_PATH)/recovery/root/system/bin/android.hardware.keymaster@3.0-service-qti
